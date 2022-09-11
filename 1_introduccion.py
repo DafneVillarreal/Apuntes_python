1114a4db99cf09e619673fca244aacd7e52c4b44
Dafne Villarreal <83681690+DafneVillarreal@users.noreply.github.com>
2022-09-11 00:48:30 -0500

    Create 1_introduccion.py

---
+++ 1_introduccion.py
@@ -0,0 +1,25 @@
+"""
+Python es un lenguaje de alto nivel, interpretado, no es necesario compilar,
+solamente lo ejecutamos.
+
+*Es muy legible para el humano
+*Multiproposito
+*Multiparadigma, podemos trabajar con objetos o lineal sin problema
+*Opensource
+
+ ¿Para qué nos sirve python?
+    *Backend web (Flask, Django)
+    *Desarrollo de apps
+    *Machine learning
+    *Data science
+    *Aplicaciones de escritorio
+    *Networking (envío de paquetes)
+    *Herramientas para línea de comandos 
+    *Testing
+    *Web scrapping
+"""
+
+print("ola amigo")
+
+#ctrl + shift + c abriremos una terminal en el directorio en cual estamos trabajando
+#para ejecutarlo intruducimos " py <archivo>.py "
